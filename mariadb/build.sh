730e818f4e757299e0e7bab049bba3db84158e60
Eduardo Tongson <propolice@gmail.com>
2018-09-27 22:17:08 +0000

    mariadb: Use new OPTS variable.

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -2,9 +2,10 @@
 set -efu
 NAME="mariadb-build"
 FROM="debian:testing-slim"
+MARIADB_DATA="/srv/mariadb/data"
+OPTS="-v $MARIADB_DATA:/data"
 . ../include/lib
 . ../include/debian-pre
-MARIADB_DATA="/srv/mariadb/data"
 /usr/bin/rm -rf $MARIADB_DATA
 /usr/bin/mkdir -p $MARIADB_DATA
 RUN $APT_GET --no-install-recommends install mariadb-server locales
@@ -15,8 +16,8 @@ RUN /usr/sbin/locale-gen
 CLR /tmp
 CLR /var/tmp
 CLR /var/lib/apt/lists
-/usr/bin/buildah run -v $MARIADB_DATA:/data $__CONTAINER -- /usr/bin/chown mysql:mysql /data
-/usr/bin/buildah run -v $MARIADB_DATA:/data $__CONTAINER -- /usr/bin/mysql_install_db --user=mysql --basedir=/usr --datadir=/data
+RUN /usr/bin/chown mysql:mysql /data
+RUN /usr/bin/mysql_install_db --user=mysql --basedir=/usr --datadir=/data
 RUN /usr/bin/install -m 755 -o mysql -g root -d /var/run/mysqld
 CFG --entrypoint '["/usr/sbin/mysqld", "--no-defaults", "--user=mysql", "--basedir=/usr", "--datadir=/data", "--bind-address=127.0.0.1"]'
 CFG --cmd ''
