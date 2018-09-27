4794687583165ef81cd6956fefd78f24ddc46c23
Eduardo Tongson <propolice@gmail.com>
2018-09-27 22:15:26 +0000

    postgresql: Use new OPTS variable.

--- postgresql/build.sh
+++ postgresql/build.sh
@@ -5,6 +5,7 @@ FROM="debian:testing-slim"
 . ../include/lib
 . ../include/debian-pre
 POSTGRESQL_DATA="/srv/postgresql/data"
+OPTS="-v $POSTGRESQL_DATA:/data"
 /usr/bin/rm -rf $POSTGRESQL_DATA
 /usr/bin/mkdir -p $POSTGRESQL_DATA
 RUN $APT_GET --no-install-recommends install postgresql locales
@@ -15,8 +16,8 @@ RUN /usr/sbin/locale-gen
 CLR /tmp
 CLR /var/tmp
 CLR /var/lib/apt/lists
-/usr/bin/buildah run -v $POSTGRESQL_DATA:/data $__CONTAINER -- /usr/bin/chown postgres:postgres /data
-/usr/bin/buildah run -v $POSTGRESQL_DATA:/data $__CONTAINER -- /usr/bin/runuser postgres -c "/usr/lib/postgresql/10/bin/initdb --locale en_US.UTF-8 -E UTF8 -D '/data'"
+RUN /usr/bin/chown postgres:postgres /data
+RUN /usr/bin/runuser postgres -c "/usr/lib/postgresql/10/bin/initdb --locale en_US.UTF-8 -E UTF8 -D '/data'"
 CFG --entrypoint '["/usr/lib/postgresql/10/bin/postgres", "-c", "data_directory=/data", "-D", "/etc/postgresql/10/main", "-d", "5"]'
 CFG --cmd ''
 CFG --stop-signal TERM
