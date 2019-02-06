39308cc0b68ca41f6318252d9a41430666956968
Eduardo Tongson <propolice@gmail.com>
2019-02-06 05:34:11 +0000

    diceware: New module. Used for generating passphrases.

---
+++ modules/diceware/tests/insane-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m diceware -p insane [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "insane"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
