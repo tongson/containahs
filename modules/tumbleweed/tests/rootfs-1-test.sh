c3ea889550e754e32606a9c53619001a16fb4052
Eduardo Tongson <propolice@gmail.com>
2019-02-11 03:14:12 +0000

    tumbleweed: This for creating custom Tumbleweed rootfs tarballs.

---
+++ modules/tumbleweed/tests/rootfs-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m tumbleweed -p rootfs [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "rootfs"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
