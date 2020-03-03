a9b84f44e55c1884786a0a7ec6ac37cc71cfa019
Eduardo Tongson <propolice@gmail.com>
2020-03-03 15:17:29 +0800

    phpstan: Pass command line arguments.

--- bin/phpstan.sh
+++ bin/phpstan.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p /srv/phpstan
-podman run --name phpstan -v /srv/phpstan:/app --rm --net host --security-opt apparmor=unconfined --workdir /app phpstan
+podman run --name phpstan -v /srv/phpstan:/app --rm --net host --security-opt apparmor=unconfined --workdir /app phpstan "$@"
