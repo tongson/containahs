354e951681483b13ce6309a2bd55b2f979a3a462
Eduardo Tongson <propolice@gmail.com>
2020-04-29 12:42:30 +0000

    Sync with upstream.

--- bin/buildah.lua
+++ bin/buildah.lua
@@ -10,17 +10,19 @@ local M = {}
 local USER = os.getenv "USER"
 local HOME = os.getenv "HOME"
 --++ # BUILDAH MODULE
---++ ## buildah.from(base, main, [cwd])
+--++ ## buildah.from(base, [assets])
 --++ Returns a function that executes the *main* `buildah` routine containing the `buildah` DSL.
---++  
+--++
 --++ *base* is a required string indicating the container image to base from. (e.g. `docker://docker.io/library/debian:stable-slim`)
---++ *cwd* is an optional string that sets the current working directory for the subsequent `buildah` commands.
---++  
+--++ *assets* is an optional string that corresponds to the assets directory.
+--++
 --++ # DSL
-local from = function(base, fn, cwd, name)
-    cwd = cwd or "."
+local from = function(base, assets, name)
+    assets = assets or "."
+    local dir = "./buildah"
+    local util_buildah = "/____util-buildah"
+
     local popen = exec.ctx()
-    popen.cwd = cwd
     popen.env = { USER = USER, HOME = HOME }
     if not name then
         popen("buildah rm -a")
@@ -31,39 +33,62 @@ local from = function(base, fn, cwd, name)
     else
         msg.ok(F("Reusing %s.", name))
     end
+
+    popen("buildah add %s '%s/util-buildah.tar.xz' '%s'", name, dir, util_buildah)
+    msg.ok"Copied util-buildah executables to container root."
+
+    local mount do
+        local _, res = popen("buildah mount --notruncate %s", name)
+        mount = res.output[1]
+    end
+
+    local rm_util_buildah = function()
+	popen("test -d %s/%s", mount, util_buildah)
+	popen("rm -rf %s/%s", mount, util_buildah)
+	popen("buildah unmount %s", name)
+    end
+
     local env = {}
     setmetatable(env, {__index = function(_, value)
-        return rawget(env, string.lower(value)) or rawget(_G, string.lower(value))
+        return rawget(env, value) or rawget(_G, value)
     end})
     --++ ### RUN(command)
     --++ Runs the *command* within the container.
-    --++  
-    env.run = function(a)
+    --++
+    env.RUN = function(a)
         msg.debug("RUN %s", a)
         popen("buildah run %s -- %s", name, a)
     end
     --++ ### SCRIPT(file)
     --++ Runs the *file* within the container as a shell script.
-    --++  
-    env.script = function(a)
+    --++
+    env.SCRIPT = function(a)
         msg.debug("SCRIPT %s", a)
-        popen("buildah copy %s %s /%s", name, a, a)
+        popen("buildah copy %s %s/%s /%s", name, assets, a, a)
         popen("buildah run %s -- sh /%s", name, a)
-        popen("buildah run %s -- rm -f /%s", name, a)
+        popen("buildah run %s -- %s/rm /%s", name, util_buildah, a)
     end
     --++ ### APT_GET(arguments)
     --++ Wraps the /Debian/ `apt-get` command.
     --++ Usually used installing packages (.e.g. `APT_GET install build-essential`)
-    --++  
-    env.apt_get = function(a)
-        local apt = [[/usr/bin/env LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get -qq --no-install-recommends -o APT::Install-Suggests=0 -o APT::Get::AutomaticRemove=1 -o Dpkg::Use-Pty=0 -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold']]
+    --++
+    env.APT_GET = function(a)
+        local apt = [[/usr/bin/env LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get -qq --no-install-recommends -o APT::Install-Suggests=0 -o APT::Get::AutomaticRemove=1 -o Dpkg::Use-Pty=0 -o Dpkg::Options::='--force-confnew' -o DPkg::options::='--force-unsafe-io']]
         msg.debug("RUN apt-get %s", a)
         popen("buildah run %s -- %s %s", name, apt, a)
     end
+    --++ ### APT_PURGE(arguments)
+    --++ Wraps dpkg command to purge a package.
+    --++
+    env.APT_PURGE = function(a)
+        local dpkg = [[dpkg --purge --no-triggers --force-remove-essential --force-breaks --force-unsafe-io]]
+        msg.debug("RUN dpkg --purge %s", a)
+        popen("buildah run %s -- %s %s", name, dpkg, a)
+    end
     --++ ### ZYPPER(arguments)
     --++ Wraps the /openSUSE/ `zypper` command.
-    --++  
-    env.zypper = function(a)
+    --++
+    env.ZYPPER = function(a)
 	local z = [[/usr/bin/zypper --non-interactive --quiet]]
 	msg.debug("RUN zypper %s", a)
 	popen("buildah run %s -- %s %s", name, z, a)
@@ -71,65 +96,54 @@ local from = function(base, fn, cwd, name)
     --++ ### COPY(source, destination)
     --++ Copies the *source* file from the current directory to the the optional argument *destination*.
     --++ Writes to the root('/') directory if *destination* is not given.
-    --++  
-    env.copy = function(src, dest)
-        dest = dest or '/'
+    --++
+    env.COPY = function(src, dest)
+        dest = dest or '/'..src
         msg.debug("COPY '%s' to '%s'", src, dest)
-        popen("buildah copy %s %s %s", name, src, dest)
+        popen("buildah copy %s %s/%s %s", name, assets, src, dest)
     end
-    --++ ### CLEAR(directory)
-    --++ Deletes all files and directories one level down from the string *directory*.
-    --++ If a list(table) is given, then each directory(string) is cleared.
-    --++  
-    env.clear = function(d)
-        if type(d) == "table" and next(d) then
-            msg.debug("CLEAR (table)")
-            for _, r in ipairs(d) do
-                popen("buildah run %s -- /usr/bin/find %s -mindepth 1 -ignore_readdir_race -delete", name, r)
-            end
-        else
-            msg.debug("CLEAR %s", d)
-            popen("buildah run %s -- /usr/bin/find %s -mindepth 1 -ignore_readdir_race -delete", name, d)
-        end
-    end
-    --++ ### MKDIR(directory)
+    --++ ### MKDIR(directory, [mode])
     --++ Create directory within container.
-    --++  
-    env.mkdir = function(d)
+    --++
+    --++ Optional directory mode in octal. Default is 0755.
+    --++
+    env.MKDIR = function(d, m)
+        m = m or ""
         msg.debug("MKDIR %s", d)
-        popen("buildah run %s -- mkdir -p %s", name, d)
+        popen("buildah run %s -- %s/mkdir %s %s", name, util_buildah, d, m)
     end
     --++ ### RM(file)
     --++ Deletes the string *file*.
     --++ If a list(table) is given, then each file(string) is deleted.
-    --++  
-    env.rm = function(f)
+    --++
+    env.RM = function(f)
         if type(f) == "table" and next(f) then
             msg.debug("RM (table)")
             for _, r in ipairs(f) do
-                popen("buildah run %s -- rm -rf %s", name, r)
+                popen("buildah run %s -- %s/rm %s", name, util_buildah, r)
             end
         else
             msg.debug("RM %s", f)
-            popen("buildah run %s -- rm -rf %s", name, f)
+            popen("buildah run %s -- %s/rm %s", name, util_buildah, f)
         end
     end
     --++ ### ENTRYPOINT(executable)
     --++ Sets the container entrypoint.
     --++ NOTE: Only accepts a single entrypoint item, usually the executable.
-    --++  
-    env.entrypoint = function(s)
+    --++
+    env.ENTRYPOINT = function(s)
         msg.debug("ENTRYPOINT %s", s)
         popen("buildah config --entrypoint '[\"%s\"]' %s", s, name)
         popen("buildah config --cmd '' %s", name)
         popen("buildah config --stop-signal TERM %s", name)
     end
+    env.START = env.ENTRYPOINT
     --++ ### SSHD(argument)
     --++ Sets the container entrypoint to `sshd`.
     --++ If *argument* is a string, it will be used as the `sshd_config` by `sshd`.
     --++ If *argument* is a number, then it is considered the localhost(127.0.0.1) port number `sshd` listens to.
-    --++  
-    env.sshd = function(p)
+    --++
+    env.SSHD = function(p)
         local s
         if type(p) == "string" then
             msg.debug("SSHD file:%s", p)
@@ -145,44 +159,29 @@ local from = function(base, fn, cwd, name)
         popen("buildah config --cmd '' %s", name)
         popen("buildah config --stop-signal TERM %s", name)
     end
-    --++ ### DROPBEAR(port)
-    --++ Sets the container entrypoint to `dropbear`.
-    --++ The argument *port* is the localhost(127.0.0.1) port number `dropbear` listens to.
-    --++  
-    env.dropbear = function(p, old)
-        p = tostring(p)
-        msg.debug("DROPBEAR localhost:%s", p)
-        local s
-        if old then
-            s = F('["/usr/sbin/dropbear", "-b", "/etc/banner", "-FEm", "-p", "127.0.0.1:%s"]', p)
-        else
-            s = F('["/usr/sbin/dropbear", "-R", "-F", "-E", "-B", "-j", "-k", "-p", "127.0.0.1:%s", "-b", "/etc/banner"]', p)
-        end
-        popen("buildah config --entrypoint '%s' %s", s, name)
-        popen("buildah config --cmd '' %s", name)
-        popen("buildah config --stop-signal TERM %s", name)
-    end
     --++ ### WRITE(directory)
     --++ Writes the container image to *directory*.
     --++ > NOTE: This finalizes the `buildah` run.
-    --++  
-    env.write = function(cname)
-        msg.debug("WRITE containers-storage:%s", cname)
+    --++
+    env.WRITE = function(cname)
+        msg.debug("WRITE dir:%s", cname)
+        rm_util_buildah()
         local tmpname = F("%s.%s", cname, util.random_string(16))
         popen("buildah commit --rm --squash %s dir:%s", name, tmpname)
         popen([[mv $(find %s -maxdepth 1 -type f -exec file {} \+ | awk -F\: '/archive/{print $1}') %s.tar]], tmpname, tmpname)
         popen("mkdir %s", cname)
-        popen(F("tar -C %s -xvf %s.tar", cname, tmpname))
-        os.execute(F("rm -f %s.tar", tmpname))
-        os.execute(F("rm -rf %s", cname))
+        popen("tar -C %s -xvf %s.tar", cname, tmpname)
+        popen("rm -f %s.tar", tmpname)
+        popen("rm -rf %s", tmpname)
         msg.ok("Wrote dir:%s", cname)
     end
     --++ ### ARCHIVE(name)
     --++ Saves the container as an `oci-archive` with filename *name*.
     --++ > NOTE: This finalizes the `buildah` run.
-    --++  
-    env.archive = function(cname)
+    --++
+    env.ARCHIVE = function(cname)
         msg.debug("ARCHIVE oci:%s", cname)
+        rm_util_buildah()
         popen("buildah commit --rm --squash %s oci-archive:%s", name, cname)
         msg.ok("OCI image %s", cname)
     end
@@ -190,22 +189,25 @@ local from = function(base, fn, cwd, name)
     --++ Saves the container to `containers-storage`.
     --++ Aliases: STORAGE, COMMIT
     --++ > NOTE: This finalizes the `buildah` run.
-    --++  
-    env.containers_storage = function(cname, tag)
+    --++
+    env.CONTAINERS_STORAGE = function(cname, tag)
         tag = tag or "latest"
         msg.debug("CONTAINERS-STORAGE %s:%s", cname, tag)
+        rm_util_buildah()
         popen("buildah commit --rm --squash %s containers-storage:%s:%s", name, cname, tag)
         msg.ok("Committed image %s", cname)
     end
-    env.storage = env.containers_storage
-    env.commit = env.containers_storage
+    env.STORAGE = env.CONTAINERS_STORAGE
+    env.COMMIT = env.CONTAINERS_STORAGE
+    env.STORE = env.CONTAINERS_STORAGE
     --++ ### ECR_PUSH(repository, name, tag)
     --++ Push container to AWS ECR under *name:tag*.
     --++ Requires `aws-cli` and AWS ECR credentials.
     --++ > NOTE: This finalizes the `buildah` run.
-    --++  
-    env.ecr_push = function(repo, cname, tag)
+    --++
+    env.ECR_PUSH = function(repo, cname, tag)
         msg.debug("PUSH %s:%s", cname, tag)
+        rm_util_buildah()
         local tmpname = F("%s.%s", cname, util.random_string(16))
         popen("buildah commit --format docker --squash --rm %s dir:%s", name, tmpname)
         local _, r = popen("/usr/bin/aws ecr get-login")
@@ -220,21 +222,43 @@ local from = function(base, fn, cwd, name)
     --++ Only supports docker repository basic authentication.
     --++ Alias: PUSH
     --++ > NOTE: This finalizes the `buildah` run.
-    --++  
-    env.local_push = function(repo, creds, cname, tag)
+    --++
+    env.LOCAL_PUSH = function(repo, creds, cname, tag)
         msg.debug("PUSH %s:%s", cname, tag)
+        rm_util_buildah()
         local tmpname = F("%s.%s", cname, util.random_string(16))
         popen("buildah commit --format docker --squash --rm %s dir:%s", name, tmpname)
-        popen("/usr/bin/skopeo copy --dcreds %s dir:%s %s/%s:%s", creds, tmpname, repo, cname, tag)
+        popen("/usr/bin/skopeo copy --dcreds %s dir:%s docker://%s/%s:%s", creds, tmpname, repo, cname, tag)
         popen("/usr/bin/skopeo copy dir:%s containers-storage:%s:%s", tmpname, cname, tag)
-        os.execute(F("rm -r %s/%s", cwd, tmpname))
+        popen("rm -r %s", tmpname)
         msg.ok("Pushed %s:%s", cname, tag)
     end
-    env.push = env.local_push
+    env.PUSH = env.LOCAL_PUSH
+    --++ ### XPUSH(repository, credentials, name, tag)
+    --++ Push container to specified docker repository under *name:tag*
+    --++ Only supports docker repository basic authentication.
+    --++ Alias: PUSH
+    --++ > NOTE: This finalizes the `buildah` run.
+    --++
+    env.XPUSH = function(cname, tag, ...)
+        tag = tag or "latest"
+        msg.debug("PUSH %s:%s", cname, tag)
+        local repo = os.getenv("BUILDAH_REPO")
+        local creds = os.getenv("BUILDAH_CRED")
+        rm_util_buildah()
+        local tmpname = F("%s.%s", cname, util.random_string(16))
+        popen("buildah commit --format docker --squash --rm %s dir:%s", name, tmpname)
+        popen("/usr/bin/skopeo copy --dest-tls-verify=false --dest-creds %s dir:%s docker://%s/%s:%s", creds, tmpname, repo, cname, tag)
+        for _, newtag in ipairs{...} do
+            popen("/usr/bin/skopeo copy --src-tls-verify=false --dest-tls-verify=false --src-creds %s --dest-creds %s docker://%s/%s:%s docker://%s/%s:%s", creds, creds, repo, cname, tag, repo, cname, newtag)
+        end
+        popen("rm -r %s", tmpname)
+        msg.ok("Pushed %s:%s", cname, tag)
+    end
     -- ### CACHE(host, name, src, dest)
     -- Copy container image from `containers-storate` as `oci-archive` via `scp` to *host*.
     -- *src* and *dest* are optional image tags. *src* defaults to "latest" and *dest* defaults to *src*.
-    env.cache = function(ssh, cname, stag, dtag)
+    env.CACHE = function(ssh, cname, stag, dtag)
         stag = stag or "latest"
         dtag = dtag or stag
         msg.debug("CACHE %s:%s -> %s:%s", cname, stag, cname, dtag)
@@ -247,10 +271,29 @@ local from = function(base, fn, cwd, name)
         popen("rm IMAGE.tar.xz")
         os.execute(F("rm -r %s/%s", cwd, tmpname))
     end
-    return function(...)
-        setfenv(fn, env)
-        return fn(...)
+    env.WIPE = function(a)
+        if a == "directories" then
+            msg.debug("WIPE (recreating empty directories)")
+            popen("buildah run %s -- %s/wipe_dirs", name, util_buildah)
+        end
+        if a == "debian" then
+            msg.debug("WIPE (removing apt/dpkg and dependencies)")
+            popen("buildah run %s -- %s/wipe_debian", name, util_buildah)
+        end
+        if a == "perl" then
+            msg.debug("WIPE (removing perl)")
+            popen("buildah run %s -- %s/wipe_perl", name, util_buildah)
+        end
+        if a == "userland" then
+            msg.debug("WIPE (removing userland)")
+            popen("buildah run %s -- %s/wipe_userland", name, util_buildah)
+        end
+        if a == "docs" then
+            msg.debug("WIPE (documentation)")
+            popen("buildah run %s -- %s/wipe_docs", name, util_buildah)
+        end
     end
+    setfenv(2, env)
 end
 
 M.from = from
