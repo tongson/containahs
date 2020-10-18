08db3c760cef4b5735bc4b8d6cad7f276b8ecfe3
Eduardo Tongson <propolice@gmail.com>
2020-10-18 23:44:20 +0800

    CFLAGS and configure flag.

--- zeek/build.sh
+++ zeek/build.sh
@@ -1,8 +1,9 @@
 cd /zeek-3.2.2
-CFLAGS='-O3 -DNDEBUG -march=nocona -mtune=haswell -msse4.2' CXXFLAGS='-O3 -DNDEBUG -march=nocona -mtune=haswell -msse4.2' ./configure --build-type=none --prefix=/opt/zeek \
+env CFLAGS='-O3 -DNDEBUG -march=nocona -mtune=haswell -msse4.2 -fomit-frame-pointer -pipe' CXXFLAGS='-O3 -DNDEBUG -march=nocona -mtune=haswell -msse4.2 -fomit-frame-pointer -pipe' ./configure --build-type=none --prefix=/opt/zeek \
   --disable-broker-tests \
   --disable-zeekctl \
   --disable-auxtools \
-  --disable-python
+  --disable-python \
+  --enable-static-binpac
 make -j 1
 make install
